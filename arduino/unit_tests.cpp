62f2e3561785f80fa48c6413b7c6e8fcf2549306
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-10-07 16:34:46 -0400

    Freed alloc'd tests and added unit test for finding memory leaks

--- arduino/unit_tests.cpp
+++ arduino/unit_tests.cpp
@@ -70,11 +70,19 @@ void test_scheduler(){
   char delta;
   long goal;
   Link *link;
-  int i;
+  int i, free_memory;
+
+  // INITALIZE MEMORY TEST
+  free_memory = ESP.getFreeHeap();
+
+  // SCHEDULER_CURRENT_DATE (eyeball this one)
 
   date = scheduler_current_date();
+  Serial.printf("scheduler_current_date: (%d-%d-%d %d:%d:%d [%d])\n", date->year, (int)date->month, (int)date->day, (int)date->hour, (int)date->minute, (int)date->second, (int)date->weekday);
 
-  Serial.println("Starting task create");
+
+  // CREATE TASKS
+
   task_second = scheduled_task_create(0x84210842108421ULL, 0, 0, 0, 0, 0, 0x0, 0x0, 0x0); // Every 5 seconds in binary
   task_minute_one = scheduled_task_create(0, 0xFFFFFFFFFFFFFFFFULL, 0, 0, 0, 0, 0x0, 0x0, 0x0);
   task_minute_five = scheduled_task_create(0, 0x84210842108421ULL, 0, 0, 0, 0, 0x0, 0x0, 0x0);
@@ -91,13 +99,8 @@ void test_scheduler(){
   task_month_two = scheduled_task_create(0, 0, 0, 0, 0, 0x00000800, 0x0, 0x0, 0x0); // december
   task_month_one_fifthteen = scheduled_task_create(0, 0, 0, 0x4000, 0, 1<<(date->month), 0x0, 0x0, 0x0); // november
   task_month_two_fifthteen = scheduled_task_create(0, 0, 0, 0x4000, 0, 1<<(date->month+1), 0x0, 0x0, 0x0); // november
-
-  // SCHEDULER_CURRENT_DATE (eyeball this one)
-
-  // Prints current date
-  Serial.printf("scheduler_current_date: (%d-%d-%d %d:%d:%d [%d])\n", date->year, (int)date->month, (int)date->day, (int)date->hour, (int)date->minute, (int)date->second, (int)date->weekday);
   
-  
+
   // SCHEDULER_DELTA_VECTOR
 
   // Target is left of the cursor
@@ -112,12 +115,10 @@ void test_scheduler(){
 
   // SCHEDULER_NEAREST_BIT
 
-  // Checks which bit is lowest
   test_run_long("test_scheduler_nearest_bit", 5,  scheduler_nearest_bit(1<<5));
 
 
   // SCHEDULER_DELTA
-  Serial.println("Testing scheduler_delta.");
   test_run_long("test_scheduler_delta_second_5", timeClient.getEpochTime()%5 ? 5-(timeClient.getEpochTime()%5) : 5, scheduler_delta(task_second));
   test_run_long("test_scheduler_delta_minute_1", 60-date->second, scheduler_delta(task_minute_one));
   test_run_long("test_scheduler_delta_minute_5", 300-((date->minute%5)*60)-date->second, scheduler_delta(task_minute_five));
@@ -143,6 +144,7 @@ void test_scheduler(){
   goal = (seconds_in_day-date->hour*60*60-date->minute*60-date->second) + seconds_in_day*(days_in_month[date->month-1]-date->day) + (days_in_month[date->month]+14)*seconds_in_day;
   test_run_long("test_scheduler_delta_month_two_fifthteen", goal, scheduler_delta(task_month_two_fifthteen));
 
+
   // SCHEDULER INSERT
   queue = scheduler_create();
   scheduler_insert(queue, task_minute_one);
@@ -160,8 +162,21 @@ void test_scheduler(){
   if (task_second) scheduled_task_free(task_second);
   if (task_minute_one) scheduled_task_free(task_minute_one);
   if (task_minute_five) scheduled_task_free(task_minute_five);
+  if (task_hour_one) scheduled_task_free(task_hour_one);
+  if (task_hour_two) scheduled_task_free(task_hour_two);
+  if (task_hour_less) scheduled_task_free(task_hour_less);
+  if (task_hour_greater) scheduled_task_free(task_hour_greater);
+  if (task_day_one) scheduled_task_free(task_day_one);
+  if (task_day_two) scheduled_task_free(task_day_two);
+  if (task_day_before) scheduled_task_free(task_day_before);
+  if (task_day_fifthteen) scheduled_task_free(task_day_fifthteen);
+  if (task_month_tuesday) scheduled_task_free(task_month_tuesday);
+  if (task_month_one) scheduled_task_free(task_month_one);
+  if (task_month_two) scheduled_task_free(task_month_two);
+  if (task_month_one_fifthteen) scheduled_task_free(task_month_one_fifthteen);
+  if (task_month_two_fifthteen) scheduled_task_free(task_month_two_fifthteen);
   
   if (date) date_free(date);
   // Final memory check
-  Serial.printf("Final free heap: %d bytes\n", ESP.getFreeHeap());
+  test_run_long("test_scheduler_heap_check", free_memory, ESP.getFreeHeap());
 }
